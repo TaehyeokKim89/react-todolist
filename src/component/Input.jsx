8bafd3663c3f807986ee7e5bb9f2025f9b89b736
VIVID89 <wskth0930@gmail.com>
2023-02-07 22:43:48 +0900

    css 기능 추가

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -6,6 +6,7 @@ function Input({title,onTitleHandler,onDescHandler,desc,clickAddButtonHandler}) 
         <div>
             <label className="inputName"> 제목 </label>
             <input
+                placeholder='할 일을 입력 해주세요!'
                 type="text"
                 id="title"
                 className="inputBoxIn"
@@ -14,6 +15,7 @@ function Input({title,onTitleHandler,onDescHandler,desc,clickAddButtonHandler}) 
             ></input>
             <label className="inputName"> 내용 </label>
             <input
+                placeholder='할 일을 자세히 입력 해주세요!'
                 type="text"
                 id="desc"
                 className="inputBoxIn"
@@ -23,7 +25,7 @@ function Input({title,onTitleHandler,onDescHandler,desc,clickAddButtonHandler}) 
         </div>
     <div className="button">
         <button onClick={clickAddButtonHandler} className="addButton">
-            추가하기
+             추가하기 
         </button>
     </div>
 </div>
